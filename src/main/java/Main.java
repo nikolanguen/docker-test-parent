8fcf17315e2443cd00df7bfcbd34637d6eeba603
Georgi Ivanov1 <georgi_ivanov1@epam.com>
2021-03-10 13:11:01 +0200

    Add initial project.

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        System.out.println("Test parent running");
+        Class<?> clazz = Class.forName("test.child.Task");
+        Method multiplyMethod = clazz.getMethod("multiply", int.class, int.class);
+        Integer result = (Integer) multiplyMethod.invoke(null, 3,3);
+        System.out.println("Multiply result: " + result);
+    }
+}
