5e312ebd79ae026db81affd5f882cac2551e6ae1
Georgi Ivanov1 <georgi_ivanov1@epam.com>
2021-03-23 12:03:35 +0200

    Update files.

--- src/main/java/test/parent/Main.java
+++ src/main/java/test/parent/Main.java
@@ -3,6 +3,7 @@ package test.parent;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Result multiply: " + Solution.multiply(5,5));
         System.out.println("Result division: " + SolutionTwo.divide(10,2));
     }
--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -1,15 +1,10 @@
-import org.junit.internal.TextListener;
 import org.junit.platform.launcher.Launcher;
 import org.junit.platform.launcher.LauncherDiscoveryRequest;
-import org.junit.platform.launcher.TestPlan;
 import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
 import org.junit.platform.launcher.core.LauncherFactory;
 import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
 import org.junit.platform.launcher.listeners.TestExecutionSummary;
-import org.junit.runner.JUnitCore;
-import test.parent.Solution;
 import test.parent.SolutionTest;
-import test.parent.SolutionTwo;
 
 import java.io.PrintWriter;
 
@@ -23,7 +18,6 @@ public class TestMain {
                 .selectors(selectClass(SolutionTest.class))
                 .build();
         Launcher launcher = LauncherFactory.create();
-        TestPlan testPlan = launcher.discover(request);
         launcher.registerTestExecutionListeners(listener);
         launcher.execute(request);
         return listener;
@@ -33,7 +27,5 @@ public class TestMain {
         SummaryGeneratingListener listener = runTest();
         TestExecutionSummary summary = listener.getSummary();
         summary.printTo(new PrintWriter(System.out));
-        System.out.println("Result multiply: " + Solution.multiply(5,5));
-        System.out.println("Result division: " + SolutionTwo.divide(10,2));
     }
 }
